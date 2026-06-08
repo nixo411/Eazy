97a3e56e56c579cfa854ae61382f87ed2aa14a53
Nam0 <gianni.blazquez@gmail.com>
2026-06-08 17:38:25 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+// À COMPLÉTER : Modifie les valeurs pour personnaliser ton site
+const CONFIG = {
+    title: "Eazy Community",
+    description: "Remplace ce texte par la description de ta communauté. Partage, échange et progresse avec nous !",
+    discordLink: "https://discord.gg/TON-LIEN-ICI",
+    
+    // Liste des avantages (laisse vide [] si tu n'en veux pas)
+    features: [
+        "Entraide et Partage",
+        "Événements réguliers",
+        "Staff à l'écoute"
+    ]
+};
